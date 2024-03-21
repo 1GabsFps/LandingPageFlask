98d141301eeee4bf874e2ff0894a9c1c8a16cd6f
1GabsFps <gabriel.neco@live.com>
2024-03-21 10:29:22 -0300

    versão em sql

--- main.py
+++ main.py
@@ -1,58 +1,67 @@
 from flask import Flask, render_template, request, redirect
+import mysql.connector as sql
 import csv
-import os 
+import os
+
+connection = sql.connect(host="127.0.0.1", user="root", password="", database="logins")
+
+cursor = connection.cursor()
+
 app = Flask(__name__)
 
-app.config['STATIC_FOLDER'] = 'static'
-app.config
-if not os.path.exists('products.csv'):
-    with open('products.csv', 'w', newline='') as f:
+app.config["STATIC_FOLDER"] = "static"
+if not os.path.exists("products.csv"):
+    with open("products.csv", "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerow(['Produto', 'Preco', 'Estoque'])
-if not os.path.exists('users.csv'):
-    with open('users.csv', 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(['Nome', 'Senha'])
+        writer.writerow(["Produto", "Preco", "Estoque"])
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return redirect('/login')
+    return redirect("/login")
 
-@app.route('/list', methods=['GET', 'POST'])
+
+@app.route("/list", methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
-        product = request.form['product']
-        price = float(request.form['price'])
-        stock = int(request.form['stock'])
-        with open('products.csv', 'a', newline='') as f:
+    if request.method == "POST":
+        product = request.form["product"]
+        price = float(request.form["price"])
+        stock = int(request.form["stock"])
+        with open("products.csv", "a", newline="") as f:
             writer = csv.writer(f)
             writer.writerow([product, price, stock])
-        return redirect('/list')
-    with open('products.csv', 'r') as f:
+        return redirect("/list")
+    with open("products.csv", "r") as f:
         products = [row for row in csv.reader(f)]
-    return render_template('index.html', products=products)
+    return render_template("index.html", products=products)
 
-@app.route('/login', methods=['GET', 'POST'])
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        with open('users.csv', 'r') as f:
-            users = [row for row in csv.reader(f)]
-        for user in users:
-            if user[0] == username and user[1] == password:
-                return redirect('/list')
-    return render_template('login.html')
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        cursor.execute(
+            f"SELECT * FROM usuarios WHERE nome = '{username}' AND senha = '{password}'"
+        )
+        user = cursor.fetchall()
+        if user:
+            return redirect("/list")
+        else:
+            return redirect("/login")
+    return render_template("login.html")
 
-@app.route('/register', methods=['GET', 'POST'])
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        with open('users.csv', 'a', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow([username, password])
-        return redirect('/login')
-    
-if __name__ == '__main__':
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        cursor.execute(
+            f"INSERT INTO usuarios (nome, senha) VALUES ('{username}', '{password}')"
+        )
+        return redirect("/login")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
